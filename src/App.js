af6a4d71fde283dc677eb92351dd3b1d3e810dce
Aastha 204 <aasthagarg333@gmail.com>
2024-08-29 14:51:10 +0530

    booktype

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import LibraryManagementTextOverlay from './components/LibraryManagementTextOverlay';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import UserProfile from './components/UserProfile';
+import BookTypes from './components/booktypes';
 
 function App() {
   return (
@@ -8,9 +9,10 @@ function App() {
       <Routes>
         <Route path="/" element={<LibraryManagementTextOverlay/>}/>
         <Route path="/userProfile" element={<UserProfile/>}/>
+        <Route path="/bookTypes" element={<BookTypes/>}/>
       </Routes>
     </Router>
   );
 }
 
-export default App;
+export default App
